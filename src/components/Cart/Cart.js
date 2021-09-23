eaf4ca0040ee9e08d320f6a09c0a1e84b33dd157
Shipu8644 <shipu.monjurur@gmail.com>
2021-09-23 20:03:58 +0600

    font-awesome left

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -3,13 +3,47 @@ import './Cart.css';
 const Cart = (props) => {
     const { cart } = props;
     const total = cart.reduce((a, b) => (a + b.price), 0);
+    const shipping = total > 0 ? 15 : 0;
+    const tax = total * (15 / 100);
+    const grandTotal = total + shipping + tax;
 
     return (
-        <div className='cart'>
-            <h3>Order Summary</h3>
-            <h5>Items Ordered: {cart.length}</h5>
-            <p>Total Price:{total.toFixed(2)}</p>
-        </div>
+        <div >
+
+            <div className='cart'>
+                <table>
+                    <tbody>
+                        <div >
+                            <th><h3 >Order Summary</h3></th>
+                            <tr><th><h5>Items Ordered: {cart.length}</h5></th></tr>
+                        </div>
+
+                        <tr>
+                            <td>Total Price</td>
+                            <td>: {total.toFixed(2)}</td>
+                        </tr>
+                        <tr>
+                            <td>Shipping</td>
+                            <td>: {shipping.toFixed(2)}</td>
+                        </tr>
+                        <tr>
+                            <td>Tax</td>
+                            <td>: {tax.toFixed(2)}</td>
+
+                        </tr>
+                        <tr><td><hr style={{ width: '200%' }} /></td> </tr>
+                        <tr>
+                            <td>Whole Price</td>
+                            <td>: {grandTotal.toFixed(2)}</td>
+                        </tr>
+                    </tbody>
+
+
+                </table>
+            </div>
+
+
+        </div >
     );
 };
 
